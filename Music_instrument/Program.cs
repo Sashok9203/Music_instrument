eebafb59a3f89c3c3720f6833327cacff889481a
Sashok9203 <56803757+Sashok9203@users.noreply.github.com>
2023-04-09 13:14:10 +0300

    Добавьте файлы проекта.

---
+++ Music_instrument/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Music_instrument
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
